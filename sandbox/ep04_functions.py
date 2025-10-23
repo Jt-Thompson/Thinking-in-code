cb505bbf5133aee903350275ea680d00dd40f641
Jt-Thompson <cLAerWork72@proton.me>
2025-10-22 19:13:29 -0700

    Create ep04_functions.py

---
+++ sandbox/ep04_functions.py
@@ -0,0 +1,8 @@
+# 700 batch ep 4: functions pratcice 
+def log_entry_point(day):
+    print("-----")
+    print(" 700 batch reflection entry")
+    print("date:", day)
+
+log_entry_point("2025-10-20")
+log_entry_point("2025-10-21")
