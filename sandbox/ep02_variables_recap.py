57c059a2038f0d76da1f44171c9d7a6472ff0aa5
Jt-Thompson <cLAerWork72@proton.me>
2025-10-22 19:11:20 -0700

    Add daily recap input and summary logging

---
+++ sandbox/ep02_variables_recap.py
@@ -0,0 +1,13 @@
+day = input ("recap date (YYY-MM-DD) ,. ")
+print("OK - logging date:", day)
+
+mood = input ("mood for the day (e.g., focused, tired, engergized): ")
+print("OK - logging recap for" , day, "Mood:", mood) 
+
+summary = input ("Write a quick summary of your day: ")
+
+print("-----") 
+print("recap Summary for", day)
+print("Mood", mood)
+print("summary:", summary)
+print("-----")
